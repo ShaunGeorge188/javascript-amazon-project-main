9baa0e176a65704fe9f2ee3f882ce9c5026ac712
Shaun George <shaungeorge2006@gmail.com>
2026-01-26 15:33:29 +0530

    Move addToCart into cart.js and used modules to import cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
 //generating html using javascript.
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 let productsHTML = '';
 
@@ -62,37 +64,29 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
+
+function updateCartQuantity(){
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+
+  document.querySelector('.add-quant')
+    .innerHTML = cartQuantity;
+
+}
+
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
 
-      let matchingItem;
-      cart.forEach((item) => {
-        if(productId === item.productId){
-          matchingItem = item;
-        }
-      });
-
-      if(matchingItem){
-        matchingItem.quantity += 1;
-      } else{
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
+      addToCart(productId);
 
 
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-      
-
-      document.querySelector('.add-quant')
-        .innerHTML = cartQuantity;
-
+      updateCartQuantity();
     });
-  });
+});
